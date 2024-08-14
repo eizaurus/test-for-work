21c0104adbadaedd03be5a68ade8f33bac191bd2
eizaurus_work <eixon74@gmail.com>
2024-08-14 18:01:09 +0300

    -

--- src/app/todos.tsx
+++ src/app/todos.tsx
@@ -8,40 +8,55 @@ import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import Card from '@mui/material/Card';
-import CommentIcon from '@mui/icons-material/Comment';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/material';
-import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import AddTaskIcon from '@mui/icons-material/AddTask';
 import { action, AppD, AppS, selector } from '@/redux';
-import { taskList } from '@/redux/components/reduce';
+import { taskList, visible } from '@/redux/components/reduce';
 
 export default function Todos() {
 	const d = AppD();
 	const [task, setTask] = React.useState('');
-	const [choice, setChoice] = React.useState('All');
-
+	const choice: visible = AppS(selector.visible);
+	const [currentCount, setCount] = React.useState(0);
 	const taskList: taskList = AppS(selector.task);
-	console.log('taskList', taskList);
 	const addTask = () => {
 		if (task.length > 0) {
 			d(action.task_add(task));
 		}
 	};
-	const changeTaskComplete = () => {};
+	const changeTaskComplete = (id: number) => () => {
+		const ids = String(id);
+		d(action.task_change(ids));
+	};
+	const changeVisible = (v: visible) => {
+		d(action.visible_change(v));
+	};
+	const clearCompleted = () => {
+		d(action.clearCompleted());
+	};
+	React.useEffect(() => {
+		if (Object.keys(taskList).length > 0) {
+			let i = 0;
+			Object.keys(taskList).map((v, index) => {
+				let complete = taskList[Number(v)].complete;
+				if (
+					choice == 'All' ||
+					(choice === 'Active' && !complete) ||
+					(choice == 'Completed' && complete)
+				)
+					i++;
+			});
+			console.log('i', i);
+			setCount(i);
+		}
+	}, [taskList, choice]);
 
 	return (
 		<Card sx={{ maxWidth: 600, maxHeight: '80vh' }}>
@@ -95,50 +110,61 @@ export default function Todos() {
 						</Box>
 					</ListItem>
 
-					{Object.keys(taskList).length > 0 ? (
+					{currentCount > 0 ? (
 						Object.keys(taskList).map((v, index) => {
 							const labelId = `checkbox-list-label-${index}`;
-							console.log('Object.keys(taskList)', v);
 							let i = Number(v);
-							return (
-								<>
-									{index > 0 && (
-										<Divider
-											variant='middle'
-											component='li'
-										/>
-									)}
-									<ListItem key={index} disablePadding>
-										<ListItemButton role={undefined} dense>
-											<ListItemIcon>
-												<Checkbox
-													edge='start'
-													checked={
-														taskList[i].complete
-													}
-													color='secondary'
-													tabIndex={-1}
-													disableRipple
-													inputProps={{
-														'aria-labelledby':
-															labelId,
-													}}
-												/>
-											</ListItemIcon>
-											<ListItemText
-												id={labelId}
-												sx={{
-													textDecoration: taskList[i]
-														.complete
-														? 'line-through'
-														: 'none',
-												}}
-												primary={taskList[i].text}
+							let complete = taskList[i].complete;
+							if (
+								choice == 'All' ||
+								(choice === 'Active' && !complete) ||
+								(choice == 'Completed' && complete)
+							)
+								return (
+									<>
+										{index > 0 && (
+											<Divider
+												variant='middle'
+												component='li'
 											/>
-										</ListItemButton>
-									</ListItem>
-								</>
-							);
+										)}
+										<ListItem key={index} disablePadding>
+											<ListItemButton
+												role={undefined}
+												dense
+												onClick={changeTaskComplete(
+													index
+												)}
+											>
+												<ListItemIcon>
+													<Checkbox
+														edge='start'
+														checked={
+															taskList[i].complete
+														}
+														color='secondary'
+														tabIndex={-1}
+														disableRipple
+														inputProps={{
+															'aria-labelledby':
+																labelId,
+														}}
+													/>
+												</ListItemIcon>
+												<ListItemText
+													id={labelId}
+													sx={{
+														textDecoration:
+															taskList[i].complete
+																? 'line-through'
+																: 'none',
+													}}
+													primary={taskList[i].text}
+												/>
+											</ListItemButton>
+										</ListItem>
+									</>
+								);
 						})
 					) : (
 						<ListItem key='no-task' disablePadding>
@@ -151,14 +177,14 @@ export default function Todos() {
 			</CardContent>
 			<CardActions sx={{ justifyContent: 'space-between' }}>
 				<Typography variant='caption' component='span'>
-					2 item
+					{currentCount} item
 				</Typography>
 				<Box>
 					<Button
 						size='small'
 						color='secondary'
 						variant={choice == 'All' ? 'outlined' : 'text'}
-						onClick={() => setChoice('All')}
+						onClick={() => changeVisible('All')}
 					>
 						<Typography variant='caption' component='span'>
 							All
@@ -168,7 +194,7 @@ export default function Todos() {
 						size='small'
 						color='secondary'
 						variant={choice == 'Active' ? 'outlined' : 'text'}
-						onClick={() => setChoice('Active')}
+						onClick={() => changeVisible('Active')}
 					>
 						<Typography variant='caption' component='span'>
 							Active
@@ -178,14 +204,18 @@ export default function Todos() {
 						size='small'
 						color='secondary'
 						variant={choice == 'Completed' ? 'outlined' : 'text'}
-						onClick={() => setChoice('Completed')}
+						onClick={() => changeVisible('Completed')}
 					>
 						<Typography variant='caption' component='span'>
 							Completed
 						</Typography>
 					</Button>
 				</Box>
-				<Button size='small' color='secondary'>
+				<Button
+					size='small'
+					color='secondary'
+					onClick={() => clearCompleted()}
+				>
 					<Typography variant='caption' component='span'>
 						Clear completed
 					</Typography>
